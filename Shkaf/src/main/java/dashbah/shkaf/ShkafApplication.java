78bbaff134a2ab858ee5d8df0b1afd696b2777cf
dashbah <dsbakhankova@edu.hse.ru>
2024-03-02 20:29:14 +0300

    initial commit, database connected

---
+++ Shkaf/src/main/java/dashbah/shkaf/ShkafApplication.java
@@ -0,0 +1,13 @@
+package dashbah.shkaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShkafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShkafApplication.class, args);
+	}
+
+}
